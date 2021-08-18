c55035d615503d199320d847b8d3b63311543025
Elvis <66243414+Elvis-1@users.noreply.github.com>
2021-08-19 09:51:28 +1300

    updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Product;
+
 use App\Models\User;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
--- routes/api.php
+++ routes/api.php
@@ -19,18 +19,18 @@ use Illuminate\Support\Facades\Route;
 //  Route::resource('products', ProductController::class);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('products', [ProductController::class, 'index']);
-Route::get('/products', [ProductController::class, 'show']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
 // Protected routes
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-//     Route::post('/logout', [AuthController::class, 'logout']);
-//     Route::post('/products', [ProductController::class, 'store']);
-//     Route::put('/products', [ProductController::class, 'update']);
-//     Route::delete('/products', [ProductController::class, 'destroy']);
-// });
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products', [ProductController::class, 'store']);
 
